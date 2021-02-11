6e122d8b8c21aa9657033ae9f08184ce277e98bd
Spinzee <Lindsey.donaldson@live.com>
2021-02-11 23:45:22 +0000

    Revert "Repo and cloud set to UI shader"
    This reverts commit 618f9ec2ad863ad3bb60775eaf98774a7ea1812f.

--- CourseAnimation/Assets/Plugins/signin.bat
+++
@@ -1,3 +0,0 @@
-echo off
-title Sign In to AltspaceVR
-curl -v -d "user[email]=lindsey.donaldson@live.com&user[password]=L12ndsey12" https://account.altvr.com/users/sign_in.json -c cookie
--- CourseAnimation/Assets/Plugins/uploadKit.bat
+++
@@ -1,2 +0,0 @@
-echo off
-curl -v -b cookie -X PUT -F "kit[zip]=@D:\Repo\course-animation\CourseAnimation\1669995809623507268_vrappdevm6a.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1669995809623507268.json
